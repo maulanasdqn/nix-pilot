4df5647ace3b42d80d37e6fafe1837cbc775127d
maulanasdqn <maulanasdqn@gmail.com>
2026-02-20 15:53:11 +0700

    Fix np-ui version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -116,7 +116,7 @@
         # Process WASM with wasm-bindgen
         np-ui = pkgs.stdenv.mkDerivation {
           pname = "np-ui";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./np-ui;
 
           nativeBuildInputs = with pkgs; [
